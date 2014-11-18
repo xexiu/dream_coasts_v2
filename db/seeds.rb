6d9585c1391c0487b856cf55569fbcf48b6b55ed
ulisescontreras <indieuli@outlook.com>
2014-11-18 20:35:33 +0000

    new seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,18 +7,24 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 #
 
-beach1 = Beach.create(name: 'Begur', short_description: "Blabla", long_description: "Lorem ipsum dolor sit amet.", img: "http://playascalas.com/wp-content/2011/07/playas-de-begur-en-girona2.jpg")
+beach1 = Beach.create(name: 'Tossa de Mar', short_description: "description", long_description: "Lorem ipsum dolor sit amet.", img: "https://lh5.googleusercontent.com/kCMYWgJ0_cnmkyd3Ot_iZF7gk5tc1DG-I0eXLwNX3Fg_WSuiaWhY1Col7k9pte9bHMzfEA=w1325-h523")
 
-beach2 = Beach.create(name: 'Tossa de Mar', short_description: "Blabla", long_description: "Lorem ipsum dolor sit amet.", img: "http://playascalas.com/wp-content/2011/07/playas-de-begur-en-girona2.jpg")
+beach2 = Beach.create(name: 'Begur', short_description: "description", long_description: "Lorem ipsum dolor sit amet.", img: "https://lh5.googleusercontent.com/JtCB4PvU5sSFDHZBl7snAZlMmLJpZi5T6lqCEyBaR-tYORoAj8DRW4DPxT3igXFbCXLWvA=w1325-h523")
 
-beach3 = Beach.create(name: 'Begur', short_description: "Blabla", long_description: "Lorem ipsum dolor sit amet.", img: "http://playascalas.com/wp-content/2011/07/playas-de-begur-en-girona2.jpg")
+beach3 = Beach.create(name: 'Palamós', short_description: "description", long_description: "Lorem ipsum dolor sit amet.", img: "https://lh5.googleusercontent.com/TVLPf4T6P-NkLXObomiUFtqWQ07rQyGT5EfC9TgRG1O2llyz8nPiMdU1QQ4EWzsB5ROKCQ=w1325-h523")
 
-beach4 = Beach.create(name: 'Begur', short_description: "Blabla", long_description: "Lorem ipsum dolor sit amet.", img: "http://playascalas.com/wp-content/2011/07/playas-de-begur-en-girona2.jpg")
+beach4 = Beach.create(name: 'Sant Feliu de Guixols', short_description: "description", long_description: "Lorem ipsum dolor sit amet.", img: "https://lh6.googleusercontent.com/aP8ISozzd7XC_SpjhjFmY-p-EZsPEcsSLpQ3Pck5idiABIs7AGPQDTKFIyngdYYmLJWrow=w1325-h523")
 
-beach5 = Beach.create(name: 'Begur', short_description: "Blabla", long_description: "Lorem ipsum dolor sit amet.", img: "http://playascalas.com/wp-content/2011/07/playas-de-begur-en-girona2.jpg")
+beach5 = Beach.create(name: 'Lescala', short_description: "description", long_description: "Lorem ipsum dolor sit amet.", img: "https://lh4.googleusercontent.com/4gC9QPuta2ZvciHTOWkhD1RHA2REDWqL2cn4085qj0MjE-IjeUh8vz06fGm7E-aWUBZL0Q=w1325-h523")
 
-beach6 = Beach.create(name: 'Begur', short_description: "Blabla", long_description: "Lorem ipsum dolor sit amet.", img: "http://playascalas.com/wp-content/2011/07/playas-de-begur-en-girona2.jpg")
+beach6 = Beach.create(name: 'Llafranc', short_description: "description", long_description: "Lorem ipsum dolor sit amet.", img: "https://lh3.googleusercontent.com/SA7cEd5STlowtoam4aycuqjqmhJZ_gO3ZaEcbahso_wKTrqlMZExfEqGCwKetubjqJWfgQ=w1325-h523")
 
-beach7 = Beach.create(name: 'Begur', short_description: "Blabla", long_description: "Lorem ipsum dolor sit amet.", img: "http://playascalas.com/wp-content/2011/07/playas-de-begur-en-girona2.jpg")
+beach7 = Beach.create(name: 'Platja dAro', short_description: "description", long_description: "Lorem ipsum dolor sit amet.", img: "https://lh6.googleusercontent.com/FVtjnYoVo1p3oK_DEB_XJDRiFipcLgh0Z-neJoRrnES1XlryWwqmd6XM75uMHUg1sWnazg=w1325-h523")
 
-beach8 = Beach.create(name: 'Begur', short_description: "Blabla", long_description: "Lorem ipsum dolor sit amet.", img: "http://playascalas.com/wp-content/2011/07/playas-de-begur-en-girona2.jpg")
+beach8 = Beach.create(name: 'Lloret de Mar', short_description: "description", long_description: "Lorem ipsum dolor sit amet.", img: "https://lh5.googleusercontent.com/YseVw4wlP1VVuTkwRZezm-1bUOduOziaso7mEmmN3PBAYBZ-1_60XpGPBGn5v0OCuY879g=w1325-h523")
+
+beach9 = Beach.create(name: 'Tamariu', short_description: "description", long_description: "Lorem ipsum dolor sit amet.", img: "https://lh4.googleusercontent.com/lZ1eEN3AAh-RLCw-2Bai_3R4l_pR3BMgSQ6h9jmlY4rHogz-_b3w2fpPlPgK0f44E4iIwA=w1325-h523")
+
+beach10 = Beach.create(name: 'Empuriabrava', short_description: "description", long_description: "Lorem ipsum dolor sit amet.", img: "https://lh3.googleusercontent.com/Jcnt0NYeHM86IHqpgJRsMSV76_oy2mkROcLMPIipAPUtPKGQ1sw6hkC6OMc_4Y_foSD8Hg=w1325-h523")
+
+beach11 = Beach.create(name: 'Portbou', short_description: "description", long_description: "Lorem ipsum dolor sit amet.", img: "https://lh3.googleusercontent.com/A0uHVqc_zqUXozx8Ne_uVLkIi6f-ACGj_QF5gEf2Ofe9SSkhxHxJenk-IzzVXviScd1_4Q=w1325-h523")
