4fc80d385ed749abca7b1cf53c72b59cb82f040c
Sergio Mironescu <sergiu.mironescu@gmail.com>
2014-11-19 09:31:59 +0100

    Fixed paths, added Devise (Users) + other fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,6 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  password   :string(255)
-#  created_at :datetime
-#  updated_at :datetime
-#
-
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,15 +1,3 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  password   :string(255)
-#  created_at :datetime
-#  updated_at :datetime
-#
-
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
