b7da13cfc4f5a3af0a3811d44d4ed22f9982f5a1
ulisescontreras <indieuli@outlook.com>
2014-11-20 11:55:37 +0000

    changes new style css login

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,13 @@
 module ApplicationHelper
-  def gravatar_for(user, options = { :size => 50 })
-    gravatar_image_tag(user.email.downcase, :alt => user.name,
-    :class => 'gravatar',
-    :gravatar => options)
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
   end
 end
