1a496fad6df73ee4e78af1e4092f4eb2820396b1
Sergio Mironescu <sergiu.mironescu@gmail.com>
2014-11-21 13:44:13 +0100

    User model with passwords, and user show page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141121111728) do
+ActiveRecord::Schema.define(version: 20141121121355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 20141121111728) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password"
+    t.string   "salt"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
